9b163d6be28e2b6d3b9da28f626e124604cee894
Brandon <bmaxwell921@gmail.com>
2014-04-04 22:25:53 -0500

    Shot sys...didn't check what I changed. new render system for enemies and shots

--- core/src/main/swapship/systems/ShotSys.java
+++ core/src/main/swapship/systems/ShotSys.java
@@ -1,29 +1,42 @@
 package main.swapship.systems;
 
+import main.swapship.common.Constants;
+import main.swapship.components.DamageComp;
 import main.swapship.components.FireRateComp;
-import main.swapship.components.LevelComp;
+import main.swapship.components.ShipColorsComp;
+import main.swapship.components.SingleSpriteComp;
 import main.swapship.components.SpatialComp;
+import main.swapship.components.dist.PlayerComp;
+import main.swapship.util.EntityFactory;
 
 import com.artemis.ComponentMapper;
 import com.artemis.Entity;
 import com.artemis.Filter;
 import com.artemis.systems.EntityProcessingSystem;
+import com.badlogic.gdx.graphics.Color;
 
 public class ShotSys extends EntityProcessingSystem {
 
 	private ComponentMapper<FireRateComp> frcm;
 	private ComponentMapper<SpatialComp> scm;
-	private ComponentMapper<LevelComp> lcm;
+	private ComponentMapper<DamageComp> dcm;
+	
+	private ComponentMapper<PlayerComp> pcm;
+	private ComponentMapper<SingleSpriteComp> sscm;
+	private ComponentMapper<ShipColorsComp> sccm;
 	
 	public ShotSys() {
-		super(Filter.allComponents(FireRateComp.class));
+		super(Filter.allComponents(FireRateComp.class, SpatialComp.class, DamageComp.class));
 	}
 	
 	@Override
 	public void initialize() {
 		frcm = world.getMapper(FireRateComp.class);
 		scm = world.getMapper(SpatialComp.class);
-		lcm = world.getMapper(LevelComp.class);
+		pcm = world.getMapper(PlayerComp.class);
+		dcm = world.getMapper(DamageComp.class);
+		sscm = world.getMapper(SingleSpriteComp.class);
+		sccm = world.getMapper(ShipColorsComp.class);
 	}
 	
 	@Override
@@ -33,7 +46,29 @@ public class ShotSys extends EntityProcessingSystem {
 		frc.timeLeft -= world.getDelta();
 		
 		if (frc.timeLeft <= 0) {
-			
+			spawnShot(e);
+			frc.timeLeft = frc.startTime;
 		}
+	}
+	
+	private void spawnShot(Entity e) {
+		SpatialComp sc = scm.get(e);
+		DamageComp dc = dcm.get(e);
+		boolean isPlayer = pcm.getSafe(e) != null;
+		
+		// X location should be right in the middle of whoever shot it
+		float x = sc.x + sc.width / 2 - Constants.Shot.WIDTH / 2;
+		
+		// The y is either above the ship for players, or below for enemies
+		float y = sc.y + ((isPlayer) ? sc.height : 0);
+		EntityFactory.createShot(world, x, y, dc.damage, getColor(e, isPlayer), isPlayer);
+	}
+	
+	// Gets the color of the shot, based on whether it is a player or not
+	private Color getColor(Entity e, boolean isPlayer) {
+		if (isPlayer) {
+			return sccm.get(e).topColor;
+		}
+		return sscm.get(e).tint;
 	}
 }
---
+++ core/src/main/swapship/systems/SingleSpriteRenderSys.java
@@ -0,0 +1,48 @@
+package main.swapship.systems;
+
+import main.swapship.SwapShipGame;
+import main.swapship.components.SingleSpriteComp;
+import main.swapship.components.SpatialComp;
+import main.swapship.util.AssetUtil;
+
+import com.artemis.ComponentMapper;
+import com.artemis.Entity;
+import com.artemis.Filter;
+import com.artemis.systems.EntityProcessingSystem;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class SingleSpriteRenderSys extends EntityProcessingSystem {
+
+	private ComponentMapper<SpatialComp> scm;
+	private ComponentMapper<SingleSpriteComp> sscm;
+
+	private final SwapShipGame game;
+	private OrthographicCamera camera;
+
+	public SingleSpriteRenderSys(final SwapShipGame game,
+			OrthographicCamera camera) {
+		super(Filter.allComponents(SpatialComp.class, SingleSpriteComp.class));
+		this.game = game;
+		this.camera = camera;
+	}
+
+	@Override
+	public void initialize() {
+		scm = world.getMapper(SpatialComp.class);
+		sscm = world.getMapper(SingleSpriteComp.class);
+	}
+
+	@Override
+	public void process(Entity e) {
+		SpatialComp sc = scm.get(e);
+		SingleSpriteComp ssc = sscm.get(e);
+
+		TextureRegion tr = AssetUtil.getInstance().getTexture(ssc.name);
+		game.batch.setProjectionMatrix(camera.combined);
+
+		game.batch.setColor(ssc.tint);
+		game.batch.draw(tr, sc.x, sc.y, sc.width / 2, sc.height / 2, sc.width,
+				sc.height, 1, 1, 0);
+	}
+}
