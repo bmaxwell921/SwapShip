7761967f12051574c4b88928f03c861b87756a6e
Brandon <bmaxwell921@gmail.com>
2014-04-05 09:26:50 -0500

    Preparing for path following code

---
+++ core/src/main/swapship/components/other/PathFollowComp.java
@@ -0,0 +1,20 @@
+package main.swapship.components.other;
+
+import com.artemis.Component;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+
+/**
+ * Component holding the path for an Entity to follow
+ * @author Brandon
+ *
+ */
+public class PathFollowComp implements Component {
+
+	public Array<Vector2> path;
+	
+	@Override
+	public void reset() {
+		path = null;
+	}
+}
