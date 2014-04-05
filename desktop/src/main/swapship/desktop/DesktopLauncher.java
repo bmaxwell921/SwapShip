3cffc905f26d129457ebb9398fda08f2664634e7
Brandon <bmaxwell921@gmail.com>
2014-04-04 19:46:39 -0500

    Added config

--- desktop/src/main/swapship/desktop/DesktopLauncher.java
+++ desktop/src/main/swapship/desktop/DesktopLauncher.java
@@ -5,11 +5,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
 
 import main.swapship.SwapShipGame;
+import main.swapship.common.Constants;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		packImages();
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Swap Ship";
+		config.width = Constants.SCREEN_WIDTH;
+		config.height = Constants.SCREEN_HEIGHT;
 		new LwjglApplication(new SwapShipGame(), config);
 	}
 	
