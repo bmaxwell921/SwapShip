98c127a654fb7ae9d001f8955c018a9491c1c6f2
Brandon <bmaxwell921@gmail.com>
2014-04-04 19:45:41 -0500

    Added constants file

---
+++ core/src/main/swapship/common/Constants.java
@@ -0,0 +1,13 @@
+package main.swapship.common;
+
+
+public class Constants {
+	public static final int SCREEN_WIDTH = 640;
+	public static final int SCREEN_HEIGHT = 800;
+	
+	public static class MainScreen {
+		public static final String PLAY = "PLAY";
+		public static final String SETTINGS = "SETTINGS";
+		public static final String HELP = "HELP";
+	}
+}
