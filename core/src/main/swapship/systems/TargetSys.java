37a05a20220b69f4967e03c5288cc7fa93fae975
Brandon <bmaxwell921@gmail.com>
2014-04-05 00:58:22 -0500

    Targeting stuff

---
+++ core/src/main/swapship/systems/TargetSys.java
@@ -0,0 +1,40 @@
+package main.swapship.systems;
+
+import main.swapship.components.SpatialComp;
+import main.swapship.components.TargetComp;
+import main.swapship.components.VelocityComp;
+import main.swapship.util.VectorUtil;
+
+import com.artemis.ComponentMapper;
+import com.artemis.Entity;
+import com.artemis.Filter;
+import com.artemis.systems.EntityProcessingSystem;
+import com.badlogic.gdx.math.Vector2;
+
+public class TargetSys extends EntityProcessingSystem {
+
+	private ComponentMapper<SpatialComp> scm;
+	private ComponentMapper<TargetComp> tcm;
+	private ComponentMapper<VelocityComp> vcm;
+	
+	public TargetSys() {
+		super(Filter.allComponents(SpatialComp.class, TargetComp.class, VelocityComp.class));
+	}
+	
+	@Override
+	public void initialize() {
+		tcm = world.getMapper(TargetComp.class);
+		vcm = world.getMapper(VelocityComp.class);
+	}
+	
+	@Override
+	public void process(Entity e) {
+		SpatialComp sc = scm.get(e);
+		TargetComp tc = tcm.get(e);
+		VelocityComp vc = vcm.get(e);
+		
+		Vector2 dir = VectorUtil.calcDirection(sc.x, sc.y, tc.target.x, tc.target.y);
+		vc.setXVel(dir.x * vc.maxVel);
+		vc.setYVel(dir.y * vc.maxVel);
+	}
+}
