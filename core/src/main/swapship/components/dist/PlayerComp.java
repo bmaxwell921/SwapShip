e37e8e9556cac5087626c3ad6a2cc3174568d342
Brandon <bmaxwell921@gmail.com>
2014-04-05 01:30:37 -0500

    PlayerComp was blocked?

---
+++ core/src/main/swapship/components/dist/PlayerComp.java
@@ -0,0 +1,18 @@
+package main.swapship.components.dist;
+
+import com.artemis.Component;
+
+/**
+ * Empty component used to distinguish players from everything else
+ * @author Brandon
+ *
+ */
+public class PlayerComp implements Component {
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
