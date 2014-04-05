091f53efee212ff5ee4dbcb0f85aa470c3227825
Brandon <bmaxwell921@gmail.com>
2014-04-04 22:24:18 -0500

    Spawning shots

--- core/src/main/swapship/util/EntityFactory.java
+++ core/src/main/swapship/util/EntityFactory.java
@@ -1,9 +1,12 @@
 package main.swapship.util;
 
 import main.swapship.common.Constants;
+import main.swapship.components.DamageComp;
+import main.swapship.components.FireRateComp;
 import main.swapship.components.LevelComp;
 import main.swapship.components.ShipColorsComp;
 import main.swapship.components.ShipSpritesComp;
+import main.swapship.components.SingleSpriteComp;
 import main.swapship.components.SpatialComp;
 import main.swapship.components.VelocityComp;
 import main.swapship.components.dist.PlayerComp;
@@ -12,6 +15,7 @@ import com.artemis.Entity;
 import com.artemis.World;
 import com.artemis.managers.GroupManager;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 
 public class EntityFactory {
 
@@ -23,12 +27,15 @@ public class EntityFactory {
 	 */
 	public static Entity createPlayer(World world) {
 		Entity e = world.createEntity();
+		
+		// Location
 		SpatialComp sc = world.createComponent(SpatialComp.class);
 		sc.setValues(Gdx.graphics.getWidth() / 2 - Constants.SHIP_WIDTH / 2,
 				Constants.Player.MIN_Y, Constants.SHIP_WIDTH,
 				Constants.SHIP_HEIGHT);
 		e.addComponent(sc);
 
+		// Drawing
 		ShipSpritesComp ssc = world.createComponent(ShipSpritesComp.class);
 		ssc.setValues(Constants.Player.ARTEMIS_TOP,
 				Constants.Player.ARTEMIS_MID, Constants.Player.ARTEMIS_BOT);
@@ -39,26 +46,58 @@ public class EntityFactory {
 				Constants.Player.DEFAULT_COLOR, Constants.Player.DEFAULT_COLOR);
 		e.addComponent(scc);
 
+		// Movement
 		VelocityComp vc = world.createComponent(VelocityComp.class);
 		vc.setValues(Constants.Player.START_VEL, Constants.Player.START_VEL);
 		e.addComponent(vc);
 
 		e.addComponent(world.createComponent(PlayerComp.class));
-		world.getManager(GroupManager.class).add(e, Constants.Groups.PLAYER);
-		e.addToWorld();
 
+		// Level
 		LevelComp lc = world.createComponent(LevelComp.class);
 		lc.setValues(Constants.Player.BASE_PART_LVL,
 				Constants.Player.BASE_PART_LVL, Constants.Player.BASE_PART_LVL,
 				Constants.Player.BASE_DAMAGE);
 		e.addComponent(lc);
+		
+		// Attacking
+		DamageComp dc = world.createComponent(DamageComp.class);
+		dc.damage = lc.calcDamage(Constants.Player.BASE_DAMAGE);
+		e.addComponent(dc);
+		
+		FireRateComp frc = world.createComponent(FireRateComp.class);
+		frc.startTime = Constants.Player.FIRE_RATE;
+		frc.timeLeft = 0;
+		e.addComponent(frc);
 
+		world.getManager(GroupManager.class).add(e, Constants.Groups.PLAYER);
+		e.addToWorld();
 		return e;
 	}
-	
-	public static void createShot(World world, int x, int y, boolean playerShot) {
+
+	public static void createShot(World world, float x, float y, int damage, Color tint,
+			boolean playerShot) {
 		Entity e = world.createEntity();
 		SpatialComp sc = world.createComponent(SpatialComp.class);
+		sc.setValues(x, y, Constants.Shot.WIDTH, Constants.Shot.HEIGHT);
+		e.addComponent(sc);
+
+		SingleSpriteComp ssc = world.createComponent(SingleSpriteComp.class);
+		ssc.name = Constants.Shot.SHOT_NAME;
+		ssc.tint = tint;
+		e.addComponent(ssc);
+
+		VelocityComp vc = world.createComponent(VelocityComp.class);
+		vc.yVel = Constants.Shot.VEL * ((playerShot) ? 1 : -1);
+		e.addComponent(vc);
+		
+		DamageComp dc = world.createComponent(DamageComp.class);
+		dc.damage = damage;
+		e.addComponent(dc);
+
+		world.getManager(GroupManager.class).add(e, playerShot ? Constants.Groups.PLAYER_ATTACK
+						: Constants.Groups.ENEMY_ATTACK);
+		e.addToWorld();
 	}
 
 }
