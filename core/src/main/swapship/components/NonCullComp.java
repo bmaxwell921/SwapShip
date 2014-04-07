5f49195375dadca807967cf4bf55c30f7cba4620
Brandon <bmaxwell921@gmail.com>
2014-04-07 14:25:06 -0500

    New component to keep stuff from being culled

---
+++ core/src/main/swapship/components/NonCullComp.java
@@ -0,0 +1,16 @@
+package main.swapship.components;
+
+import com.artemis.Component;
+
+/**
+ * Component used to keep entities from being culled
+ * @author Brandon
+ *
+ */
+public class NonCullComp implements Component {
+
+	@Override
+	public void reset() {
+		
+	}
+}
