70fc2c6a6755bc4eba3621b86c3ac7f80b80723d
Brandon <bmaxwell921@gmail.com>
2014-05-12 13:12:25 -0500

    New screen to select level and ship

---
+++ core/src/main/swapship/screens/SelectScreen.java
@@ -0,0 +1,20 @@
+package main.swapship.screens;
+
+import main.swapship.SwapShipGame;
+
+/**
+ * Screen used to select the ship and level to play
+ * @author Brandon
+ *
+ */
+public class SelectScreen extends AbstractScreen {
+
+	public SelectScreen(final SwapShipGame game) {
+		super(game);
+	}
+	
+	@Override
+	public void setUp() {
+		
+	}
+}
