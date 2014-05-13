cec54e41e3bf584e712b1b730da7679fe21ca8ee
Brandon <bmaxwell921@gmail.com>
2014-05-13 02:12:51 -0500

    Started working on Selection screen

--- core/src/main/swapship/screens/SelectScreen.java
+++ core/src/main/swapship/screens/SelectScreen.java
@@ -1,20 +1,34 @@
 package main.swapship.screens;
 
+
+
 import main.swapship.SwapShipGame;
+import main.swapship.common.Constants;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 /**
  * Screen used to select the ship and level to play
  * @author Brandon
  *
  */
-public class SelectScreen extends AbstractScreen {
-
+public class SelectScreen extends AbstractScreen { 
 	public SelectScreen(final SwapShipGame game) {
 		super(game);
 	}
 	
 	@Override
 	public void setUp() {
+		Table table = new Table();
+		table.setFillParent(true);
+		stage.addActor(table);
+		table.debug();
 		
+		final Label shipLabel = new Label("SHIPS", game.skin, Constants.UI.TITLE_LABEL);
+		table.add(shipLabel);
+		table.row();
+		
+//		final ImageButton artemisButton = new ImageButton(game.skin, styleName);
 	}
 }
