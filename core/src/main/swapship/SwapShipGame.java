4bbe89fb2686086c4ba700ad45ba7d18f4b00e13
Brandon <bmaxwell921@gmail.com>
2014-05-15 11:44:14 -0500

    Using skin from file

--- core/src/main/swapship/SwapShipGame.java
+++ core/src/main/swapship/SwapShipGame.java
@@ -6,17 +6,12 @@ import main.swapship.util.AssetUtil;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Pixmap.Format;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
 
 public class SwapShipGame extends Game {
 
@@ -34,40 +29,10 @@ public class SwapShipGame extends Game {
 		Constants.Player.MAX_Y = Gdx.graphics.getHeight() / 3;
 		this.settings = new Settings();
 		batch = new SpriteBatch();
-		createSkin();
+		skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
 		
 		this.setScreen(new MainScreen(this));
-		AssetUtil.getInstance().setTextureAtlas(new TextureAtlas(Gdx.files.internal("gameImages.atlas")));
-	}
-	
-	private void createSkin() {
-		// TODO probably should read this from a file
-		skin = new Skin();
-		
-		// Setting up fonts
-		BitmapFont titleFont = new BitmapFont();
-		titleFont.scale(2f);
-		skin.add(Constants.UI.TITLE_FONT, titleFont);	
-		skin.add(Constants.UI.OPTIONS_FONT, new BitmapFont());
-		
-		// White pixel, used later
-		Pixmap white = new Pixmap(1, 1, Format.RGBA8888);
-		white.setColor(Color.WHITE);
-		white.fill();
-		skin.add(Constants.UI.WHITE_PX, new Texture(white));
-		
-		TextButtonStyle tbs = new TextButtonStyle();
-		tbs.up = skin.newDrawable(Constants.UI.WHITE_PX, Color.DARK_GRAY);
-		tbs.down = skin.newDrawable(Constants.UI.WHITE_PX, Color.DARK_GRAY);
-		tbs.over = skin.newDrawable(Constants.UI.WHITE_PX, Color.LIGHT_GRAY);
-		tbs.font = skin.getFont(Constants.UI.OPTIONS_FONT);
-		skin.add(Constants.UI.OPTION_BUTTON, tbs);
-		
-		LabelStyle ls = new LabelStyle();
-		ls.font = skin.getFont(Constants.UI.TITLE_FONT);
-		skin.add(Constants.UI.TITLE_LABEL, ls);
-		
-		ImageButtonStyle artemisStyle = new ImageButtonStyle();
+		AssetUtil.getInstance().setTextureAtlas(new TextureAtlas(Gdx.files.internal("game/gameImages.atlas")));
 	}
 
 	@Override
